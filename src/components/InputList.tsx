4db1f26311a9837d1ec5057bba403c3e739bb379
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-02-24 09:36:51 -0300

    componente InputList

---
+++ src/components/InputList.tsx
@@ -0,0 +1,14 @@
+import { PlusCircle } from 'phosphor-react';
+import styles from './InputList.module.css';
+
+export function InputList(){
+    return (
+        <form className={styles.form} action="">
+            <input placeholder='Adicione uma nova tarefa' type="text" />
+            <button>
+                Enviar
+                <PlusCircle/>
+            </button>
+        </form>
+    )
+}
